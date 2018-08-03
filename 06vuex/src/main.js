f46550e0af269b56862affdfd3505d9a72c7e8d5
Jabo <hjbno.1@qq.com>
2018-08-03 10:20:18 +0800

    init

---
+++ 06vuex/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+import StoreConfig from './vuex/store'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  StoreConfig,
+  components: { App },
+  template: '<App/>'
+})
--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # vueKnowledgePoint
 
+``` bash
+  vuex是一个专门为vue.js设计的集中式状态管理架构。状态？我把它理解为在data中的属性需要共享给其他vue组件使用的部分，就叫做状态。简单的说就是data中需要共用的属性。
 
-## 01vuebase ： vue 基础知识点
+```
+
+## 1、安装 vuex
+
+> npm install vuex --save
 
 
-## 02components ： vue 组件
+## 2、 创建store文件
 
 
-## 03byvalue ： vue 传值
+## 3、main.js中引入的vuex文件
 
 
